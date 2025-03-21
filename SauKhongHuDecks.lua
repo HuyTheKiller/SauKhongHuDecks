95981589498321e27823e4b4451b3bed9a3cb01e
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-03-21 13:29:42 +0700

    Refactor deck effects

--- SauKhongHuDecks.lua
+++ SauKhongHuDecks.lua
@@ -16,7 +16,7 @@ SMODS.Sound({
 	key = "smash",
 	path = "smash.ogg",
 	pitch = 0.7,
-	volume = 0.3
+	volume = 0.5
 })
 
 ---------------------------------------------------------------------------------------------
@@ -910,12 +910,9 @@ SMODS.Back({
 			G.E_MANAGER:add_event(Event({
 				func = function()
 					local text = localize("k_smash_ex")
-					play_sound("gong", 0.94, 0.3)
-					play_sound("gong", 0.94 * 1.5, 0.2)
-					play_sound("tarot1", 1.5)
-					play_sound("skh_smash", 0.7, 0.3)
-					ease_colour(G.C.UI_CHIPS, { 0.8, 0.45, 0.85, 1 })
-					ease_colour(G.C.UI_MULT, { 0.8, 0.45, 0.85, 1 })
+					play_sound("multhit2", 1, 1)
+					play_sound("xchips", 1, 1)
+					play_sound("skh_smash", 0.7, 0.5)
 					attention_text({
 						scale = 1.4,
 						text = text,
@@ -924,31 +921,6 @@ SMODS.Back({
 						offset = { x = 0, y = -2.7 },
 						major = G.play,
 					})
-					G.E_MANAGER:add_event(Event({
-						trigger = "after",
-						blockable = false,
-						blocking = false,
-						delay = 4.3,
-						func = function()
-							ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
-							ease_colour(G.C.UI_MULT, G.C.RED, 2)
-							return true
-						end,
-					}))
-					G.E_MANAGER:add_event(Event({
-						trigger = "after",
-						blockable = false,
-						blocking = false,
-						no_delete = true,
-						delay = 6.3,
-						func = function()
-							G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] =
-								G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
-							G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] =
-								G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
-							return true
-						end,
-					}))
 					return true
 				end,
 			}))
@@ -1129,11 +1101,8 @@ SMODS.Back({
 			G.E_MANAGER:add_event(Event({
 				func = function()
 					local text = self.config.extra.anti_humble and localize("k_not_humbled_ex") or localize("k_humbled_ex")
-					play_sound("gong", 0.94, 0.3)
-					play_sound("gong", 0.94 * 1.5, 0.2)
-					play_sound("tarot1", 1.5)
-					ease_colour(G.C.UI_CHIPS, { 0.8, 0.45, 0.85, 1 })
-					ease_colour(G.C.UI_MULT, { 0.8, 0.45, 0.85, 1 })
+					play_sound("multhit2", 1, 1)
+					play_sound("xchips", 1, 1)
 					attention_text({
 						scale = 1.4,
 						text = text,
@@ -1142,31 +1111,6 @@ SMODS.Back({
 						offset = { x = 0, y = -2.7 },
 						major = G.play,
 					})
-					G.E_MANAGER:add_event(Event({
-						trigger = "after",
-						blockable = false,
-						blocking = false,
-						delay = 4.3,
-						func = function()
-							ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
-							ease_colour(G.C.UI_MULT, G.C.RED, 2)
-							return true
-						end,
-					}))
-					G.E_MANAGER:add_event(Event({
-						trigger = "after",
-						blockable = false,
-						blocking = false,
-						no_delete = true,
-						delay = 6.3,
-						func = function()
-							G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] =
-								G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
-							G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] =
-								G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
-							return true
-						end,
-					}))
 					return true
 				end,
 			}))
@@ -1204,11 +1148,7 @@ SMODS.Back({
 			G.E_MANAGER:add_event(Event({
 				func = function()
 					local text = G.GAME.current_round.hands_left == 0 and localize("k_complete_ex") or localize("k_not_enough_ex")
-					play_sound("gong", 0.94, 0.3)
-					play_sound("gong", 0.94 * 1.5, 0.2)
-					play_sound("tarot1", 1.5)
-					-- ease_colour(G.C.UI_CHIPS, { 0.8, 0.45, 0.85, 1 })
-					ease_colour(G.C.UI_MULT, { 0.8, 0.45, 0.85, 1 })
+					play_sound("multhit2", 1, 1)
 					attention_text({
 						scale = 1.4,
 						text = text,
@@ -1217,31 +1157,6 @@ SMODS.Back({
 						offset = { x = 0, y = -2.7 },
 						major = G.play,
 					})
-					G.E_MANAGER:add_event(Event({
-						trigger = "after",
-						blockable = false,
-						blocking = false,
-						delay = 4.3,
-						func = function()
-							-- ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
-							ease_colour(G.C.UI_MULT, G.C.RED, 2)
-							return true
-						end,
-					}))
-					G.E_MANAGER:add_event(Event({
-						trigger = "after",
-						blockable = false,
-						blocking = false,
-						no_delete = true,
-						delay = 6.3,
-						func = function()
-							-- G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] =
-								-- G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
-							G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] =
-								G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
-							return true
-						end,
-					}))
 					return true
 				end,
 			}))
@@ -1333,11 +1248,7 @@ SMODS.Back({
 				G.E_MANAGER:add_event(Event({
 					func = function()
 						local text = self.config.extra.super_generous and localize("k_super_generous_ex") or localize("k_generous_ex")
-						play_sound("gong", 0.94, 0.3)
-						play_sound("gong", 0.94 * 1.5, 0.2)
-						play_sound("tarot1", 1.5)
-						-- ease_colour(G.C.UI_CHIPS, { 0.8, 0.45, 0.85, 1 })
-						ease_colour(G.C.UI_MULT, { 0.8, 0.45, 0.85, 1 })
+						play_sound("multhit2", 1, 1)
 						attention_text({
 							scale = 1.4,
 							text = text,
@@ -1346,31 +1257,6 @@ SMODS.Back({
 							offset = { x = 0, y = -2.7 },
 							major = G.play,
 						})
-						G.E_MANAGER:add_event(Event({
-							trigger = "after",
-							blockable = false,
-							blocking = false,
-							delay = 4.3,
-							func = function()
-								-- ease_colour(G.C.UI_CHIPS, G.C.BLUE, 2)
-								ease_colour(G.C.UI_MULT, G.C.RED, 2)
-								return true
-							end,
-						}))
-						G.E_MANAGER:add_event(Event({
-							trigger = "after",
-							blockable = false,
-							blocking = false,
-							no_delete = true,
-							delay = 6.3,
-							func = function()
-								-- G.C.UI_CHIPS[1], G.C.UI_CHIPS[2], G.C.UI_CHIPS[3], G.C.UI_CHIPS[4] =
-									-- G.C.BLUE[1], G.C.BLUE[2], G.C.BLUE[3], G.C.BLUE[4]
-								G.C.UI_MULT[1], G.C.UI_MULT[2], G.C.UI_MULT[3], G.C.UI_MULT[4] =
-									G.C.RED[1], G.C.RED[2], G.C.RED[3], G.C.RED[4]
-								return true
-							end,
-						}))
 						return true
 					end,
 				}))

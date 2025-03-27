28a79b05c442f9126b4085690cb281b6148c23f5
HuyTheKiller <quochuynguyen0901@gmail.com>
2025-03-27 21:37:39 +0700

    Major code refactor

---
+++ lib/functions.lua
@@ -0,0 +1,33 @@
+function skh_get_rank_suffix(card) -- copy-pasted from Ortalab, renamed with mod id prefix for uniqueness
+    local rank_suffix = (card.base.id - 2) % 13 + 2
+    if rank_suffix < 11 then rank_suffix = tostring(rank_suffix)
+    elseif rank_suffix == 11 then rank_suffix = 'Jack'
+    elseif rank_suffix == 12 then rank_suffix = 'Queen'
+    elseif rank_suffix == 13 then rank_suffix = 'King'
+    elseif rank_suffix == 14 then rank_suffix = 'Ace'
+    end
+    return rank_suffix
+end
+
+function envious_roulette(card, odd_seed, odd_type, iteration) -- Gros Michel logic - copy-pasted and modified
+	if pseudorandom(odd_seed) < G.GAME.probabilities.normal/odd_type then
+		G.E_MANAGER:add_event(Event({
+			func = function()
+				-- play_sound('tarot1')
+				card_eval_status_text(card, 'extra', nil, nil, nil, {message = localize('k_killed_ex')})
+				card.T.r = -0.2
+				card:juice_up(0.3, 0.4)
+				card.states.drag.is = true
+				card.children.center.pinch.x = true
+				G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.3, blockable = false,
+					func = function()
+							G.jokers:remove_card(card)
+							card:remove()
+							card = nil
+						return true; end}))
+				return true
+			end
+		}))
+		iteration = iteration - 1
+	end
+end
---
+++ lib/sfx.lua
@@ -0,0 +1,27 @@
+SMODS.Sound({
+	key = "wee",
+	path = "wee.ogg",
+	pitch = 0.7,
+	volume = 0.3
+})
+
+SMODS.Sound({
+	key = "chomp",
+	path = "chomp.ogg",
+	pitch = 0.7,
+	volume = 0.3
+})
+
+SMODS.Sound({
+	key = "smash",
+	path = "smash.ogg",
+	pitch = 0.7,
+	volume = 0.5
+})
+
+SMODS.Sound({
+	key = "new_chaos_roll",
+	path = "new_chaos_roll.ogg",
+	pitch = 0.7,
+	volume = 0.5
+})
